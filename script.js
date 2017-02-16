7972f38f32de7e329bf9d0c115d4b8a5177477a4
udit04 <udiwal789@gmail.com>
2017-02-16 15:33:16 +0530

    checking113

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let script = {};
 script.hello = function (cb){
-let result ="Hello World test103";
+let result ="Hello World test104";
 cb(result);
 }
 
--- testtwo.js
+++ testtwo.js
@@ -5,7 +5,7 @@ var expect = require('chai').expect,
 describe("hello sample test",function(){
 	it("must return Hello World", function(done) {
 		base_url.hello(function(reply){
-			expect(reply).to.be.equal("Hello World test103");
+			expect(reply).to.be.equal("Hello World test104");
 			done();
 		});
     });
